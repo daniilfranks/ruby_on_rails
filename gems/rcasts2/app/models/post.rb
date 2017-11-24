0b338fc6e19d3f99e259484289e91df99e9e7e5a
Danila Babanov <danila_babanov@yahoo.com>
2017-11-24 12:41:41 +0300

    validate

--- gems/rcasts2/app/models/post.rb
+++ gems/rcasts2/app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, :description, presence: :true
+  validates :title, :description, length: { minimum: 5 }
 end
--- gems/rcasts2/spec/models/post_spec.rb
+++ gems/rcasts2/spec/models/post_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before(:each) do
+    @post = create(:post)
+  end
+
+  it 'can be created' do
+    expect(@post).to be_valid
+  end
+
+  it 'has a description instance variable' do
+    expect(@post.title).to eq('First post')
+  end
+
+  it 'has a description instance variable' do
+    expect(@post.description).to eq('my text')
+  end
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(5) }
 end
